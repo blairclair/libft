1395a870b5341a341f7ef0c1f480ba2924b6d545
Ariella Grodzinsky <agrodzin@e1z3r3p20.42.us.org>
2018-03-21 16:22:23 -0700

    fjkd

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: agrodzin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 09:47:04 by agrodzin          #+#    #+#             */
-/*   Updated: 2018/03/17 14:26:31 by agrodzin         ###   ########.fr       */
+/*   Updated: 2018/03/21 16:20:40 by agrodzin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 void	*ft_memalloc(size_t size)
 {
 	char	*str;
+	size_t	i;
 
+	i = 0;
 	str = (char*)malloc(size);
 	if (str == NULL)
 		return (NULL);
 	else
-		ft_bzero(str, size);
+		while (i < size)
+		{
+			str[i] = 0;
+			i++;
+		}
 	return (str);
 }
