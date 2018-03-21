58c23271439ee27dea98045df8969d509ddf33b2
Ariella Grodzinsky <agrodzin@e1z3r3p20.42.us.org>
2018-03-21 16:16:24 -0700

    sjk

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: agrodzin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 10:20:25 by agrodzin          #+#    #+#             */
-/*   Updated: 2018/03/21 16:00:32 by agrodzin         ###   ########.fr       */
+/*   Updated: 2018/03/21 16:16:02 by agrodzin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,22 @@ int		ft_atoi(const char *str)
 {
 	int		i;
 	int		num;
-	int		sign;
+	int		check;
 
 	i = 0;
 	num = 0;
-	sign = 1;
-	while (*(str + i) == '\n' ||
-		*(str + i) == '\t' ||
-		*(str + i) == '\r' ||
-		*(str + i) == '\v' ||
-		*(str + i) == '\f' ||
-		*(str + i) == ' ')
+	check = 1;
+	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\r' ||
+		str[i] == '\v' || str[i] == '\f' || str[i] == ' ')
 		i++;
-	if (*(str + i) == '-')
-		sign = -1;
-	if (*(str + i) == '-' || *(str + i) == '+')
+	if (str[i] == '-')
+		check = -1;
+	if (str[i] == '-' || str[i] == '+')
 		i++;
-	while (*(str + i) && *(str + i) >= '0' && *(str + i) <= '9')
-		num = num * 10 + (*(str + i++) - '0');
-	return (num * sign);
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + str[i] - '0';
+		i++;
+	}
+	return (num * check);
 }
