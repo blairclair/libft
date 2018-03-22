48b3a414ceaea936b6752d83b019e6eb877742d0
Ariella Grodzinsky <agrodzin@e1z3r3p20.42.us.org>
2018-03-21 18:01:37 -0700

    rsio

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: agrodzin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 20:51:49 by agrodzin          #+#    #+#             */
-/*   Updated: 2018/03/21 17:52:37 by agrodzin         ###   ########.fr       */
+/*   Updated: 2018/03/21 18:01:03 by agrodzin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,15 @@ int	ft_strcmp(const char *s1, const char *s2)
 
 	ss1 = (unsigned char*)s1;
 	ss2 = (unsigned char*)s2;
-	if (*ss1 == '\0' && *ss2 != '\0')
-		return (0 - *ss2);
-	if (*ss1 != '\0' && *ss2 == '\0')
-		return (*ss1);
-	while (*ss1 != '\0' && *ss2 != '\0')
+	while (*ss1 != '\0' || *ss2 != '\0')
 	{
-		if (*ss1 != *ss2)
+		if (*ss1 == *ss2)
 		{
-			if (*ss1 != '\0' && *ss2 != '\0')
-				return (*ss1 - *ss2);
+			ss1 = ss1 + 1;
+			ss2 = ss2 + 1;
 		}
-		ss1 = ss1 + 1;
-		ss2 = ss2 + 1;
+		else
+			return (*ss1 - *ss2);
 	}
-	if (*ss1 == '\0' && *ss2 != '\0')
-		return (0 - *ss2);
 	return (0);
 }
