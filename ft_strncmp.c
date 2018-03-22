164b6f13ec6f6425aff4d52457539b68596e3a77
Ariella Grodzinsky <agrodzin@e1z3r3p20.42.us.org>
2018-03-21 18:16:29 -0700

    seo

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: agrodzin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 09:58:44 by agrodzin          #+#    #+#             */
-/*   Updated: 2018/03/21 18:08:54 by agrodzin         ###   ########.fr       */
+/*   Updated: 2018/03/21 18:15:39 by agrodzin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,18 @@
 
 int	ft_strncmp(const char *s1, const char *s2, int n)
 {
-	int				num;
 	unsigned char	*ss1;
 	unsigned char	*ss2;
 
 	ss1 = (unsigned char*)s1;
 	ss2 = (unsigned char*)s2;
-	num = 0;
-	while ((*ss1 != '\0' || *ss2 != '\0') && num < n)
+	while ((*ss1 != '\0' || *ss2 != '\0') && n > 0)
 	{
 		if (*ss1 == *ss2)
 		{
 			ss1 = ss1 + 1;
 			ss2 = ss2 + 1;
-			num++;
+			n--;
 		}
 		else
 				return (*ss1 - *ss2);
