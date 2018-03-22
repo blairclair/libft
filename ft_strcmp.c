9316683ba4f6a0037b1218e127008a45aa224a4b
Ariella Grodzinsky <agrodzin@e1z3r3p20.42.us.org>
2018-03-21 17:50:07 -0700

    si

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: agrodzin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 20:51:49 by agrodzin          #+#    #+#             */
-/*   Updated: 2018/03/19 16:50:24 by agrodzin         ###   ########.fr       */
+/*   Updated: 2018/03/21 17:49:38 by agrodzin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	ft_strcmp(const char *s1, const char *s2)
 
 	ss1 = (unsigned char*)s1;
 	ss2 = (unsigned char*)s2;
-	num = 1;
 	if (*ss1 == '\0' && *ss2 != '\0')
 		return (0 - *ss2);
 	if (*ss1 != '\0' && *ss2 == '\0')
@@ -32,7 +31,6 @@ int	ft_strcmp(const char *s1, const char *s2)
 			if (*ss1 != '\0' && *ss2 != '\0')
 				return (*ss1 - *ss2);
 		}
-		num++;
 		ss1 = ss1 + 1;
 		ss2 = ss2 + 1;
 	}
